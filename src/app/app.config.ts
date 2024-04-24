497fe9131de7a3a04cef47ee44b38f07cec8c7d2
Rustam Satlykov <rustam.satlykov@proton.me>
2024-04-24 23:26:04 +0500

    feat: add taiga-ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,6 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,5 +8,5 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiKeyInterceptor } from './services/api-key.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([apiKeyInterceptor]))],
+  providers: [provideAnimations(), provideRouter(routes), provideHttpClient(withInterceptors([apiKeyInterceptor])), importProvidersFrom(TuiRootModule)],
 };
